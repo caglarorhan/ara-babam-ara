5d3c3ee223eea8b69a945494b863e589c88d8c13
caglarorhan <caglaror@gmail.com>
2024-04-07 23:08:57 -0500

    bug: PWA cache is not updating

--- js/index.js
+++ js/index.js
@@ -2,8 +2,53 @@ let aba = {
     version: '2024.0.1',
     name: 'Ara (Ba) bam (Ara)',
     target_url: 'https://www.arabam.com/',
+    defPrompt:null,
     init(){
-
+        this.registerSW();
+        this.beforeInstallPrompt();
+        this.initInstallPrompt();
+    },
+    beforeInstallPrompt(){
+        window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault();
+            this.defPrompt = e; //test
+        });
+    },
+    initInstallPrompt(){
+        const button = document.getElementById('install');
+        button.addEventListener('click', () => {
+            this.defPrompt.prompt();
+            this.defPrompt.userChoice
+                .then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    this.defPrompt = null;
+                });
+        });
+    },
+    registerSW(){
+        console.log('Registering SW');
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then((registration) => {
+                    console.log('Service Worker registered with scope: ', registration.scope);
+                    //registration.update();
+                })
+                .catch((err) => {
+                    console.log('Service Worker registration failed: ', err);
+                });
+        }
     },
 
+
 }
+
+
+window.addEventListener('load', () => {
+    aba.init();
+})
+
+
---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'v3';
+const urlsToCache = [
+    './',
+    './index.html',
+    './css/index.css',
+    './js/index.js',
+    './img/ara-babam-ara_64.png',
+    './img/ara-babam-ara_192.png',
+    './img/ara-babam-ara_512.png',
+];
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return Promise.all(urlsToCache.map(url => cache.add(url).catch(error => console.error(`Failed to cache ${url}:`, error))));
+        })
+    );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+
+                let fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    (response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
